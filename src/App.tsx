17022e3732e11824dc2c2e07be6ee9e92e700a66
Varun Pujari <varunpvp@gmail.com>
2020-11-17 23:12:03 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
   Container,
   createStyles,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   makeStyles,
   TextField,
+  Typography,
 } from "@material-ui/core";
+import { Controller, useForm } from "react-hook-form";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -22,13 +28,70 @@ const useStyles = makeStyles(() =>
 
 function App() {
   const classes = useStyles();
+  const [detailsDialogOpen, setDetailsDialogOpen] = useState(false);
+  const { control } = useForm();
+
+  const handleStartRace = () => {
+    setDetailsDialogOpen(false);
+  };
+
   return (
     <Container maxWidth="xs" className={classes.container}>
-      <TextField variant="outlined" placeholder="Race name" fullWidth />
+      <Typography variant="h5" align="center">
+        Start a Race
+      </Typography>
       <Box height={8} />
-      <Button color="primary" variant="contained" fullWidth>
+      <Typography variant="body1" align="center">
+        Race your puzzle skills
+      </Typography>
+      <Box height={16} />
+      <Controller
+        name="name"
+        control={control}
+        as={<TextField variant="outlined" placeholder="Race name" fullWidth />}
+      />
+
+      <Box height={8} />
+      <Button
+        color="primary"
+        variant="contained"
+        fullWidth
+        onClick={() => setDetailsDialogOpen(true)}
+      >
         Create Race
       </Button>
+      <Dialog
+        open={detailsDialogOpen}
+        onClose={() => setDetailsDialogOpen(false)}
+      >
+        <DialogTitle>Race Info</DialogTitle>
+        <DialogContent>
+          <Controller
+            name="time"
+            control={control}
+            as={
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Time in seconds"
+                fullWidth
+              />
+            }
+          />
+          <Controller
+            name="puzzleCount"
+            control={control}
+            as={
+              <TextField margin="dense" label="Number of puzzles" fullWidth />
+            }
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleStartRace} color="primary">
+            Start
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 }
