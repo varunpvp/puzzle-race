66c41884755e3d6ef95972f6e9ddef6c2a34b209
Varun Pujari <varunpvp@gmail.com>
2020-11-19 00:15:15 +0530

    basic flow working

--- src/pages/Race.tsx
+++ src/pages/Race.tsx
@@ -1,31 +1,95 @@
-import React, { useState } from "react";
-import { RouteComponentProps, withRouter } from "react-router-dom";
-import { puzzleList } from "../constants";
-
+import firebase from "firebase/app";
+import React, { useCallback, useEffect, useState } from "react";
+import { RouteComponentProps, useParams } from "react-router-dom";
 import PuzzleBoard from "../components/PuzzleBoard";
+import { Auth, Database } from "../config/Firebase";
+import RaceType from "../types/Race";
 
 interface Props extends RouteComponentProps<{ raceId: string }> {}
 
 const Race: React.FC<Props> = () => {
-  const [puzzles, setPuzzles] = useState(puzzleList);
+  const [race, setRace] = useState<null | RaceType>(null);
+  const params = useParams<{ raceId: string }>();
+  const [raceRef] = useState(Database.ref("race").child(params.raceId));
+
+  useEffect(() => {
+    raceRef.on("value", (snapshot) => {
+      setRace(snapshot.val());
+    });
+
+    return () => {
+      raceRef.off();
+    };
+  }, [raceRef]);
+
+  if (race === null) {
+    return <div>loading...</div>;
+  }
+
+  if (race.state === "waiting") {
+    return (
+      <div>
+        waiting for race to start{" "}
+        <button
+          onClick={() => {
+            raceRef.child("state").set("starting");
+          }}
+        >
+          Start
+        </button>
+      </div>
+    );
+  }
+
+  if (race.state === "starting") {
+    return (
+      <CountDown
+        onFinish={() => {
+          raceRef.child("state").set("started");
+        }}
+      />
+    );
+  }
+
+  if (race.state === "finished") {
+    return <div>finished</div>;
+  }
+
+  const userId = Auth.currentUser?.uid!;
+  const racer = race.racers[userId];
+  const puzzle = race.puzzleList[racer.currentPuzzleIndex];
+
+  if (racer.finishedAt) {
+    return <div>You finished the race</div>;
+  }
 
   return (
     <div>
       <PuzzleBoard
-        fen={puzzles[0].startFen}
-        solution={puzzles[0].solution}
+        fen={puzzle.startFen}
+        solution={puzzle.solution}
         movable={true}
         onIncorrectMove={() => console.log("Incorrect move")}
         onSolve={() => {
           // TODO: notify solved
 
-          setTimeout(() => {
-            if (puzzles.length === 1) {
-              // TODO: notify complete
-            } else {
-              setPuzzles(puzzles.slice(1));
-            }
-          }, 2000);
+          if (race.puzzleList.length - 1 === racer.currentPuzzleIndex) {
+            setTimeout(() => {
+              raceRef
+                .child("racers")
+                .child(userId)
+                .child("finishedAt")
+                .set(firebase.database.ServerValue.TIMESTAMP);
+            }, 1000);
+          } else {
+            setTimeout(() => {
+              raceRef
+                .child("racers")
+                .child(userId)
+                .child("currentPuzzleIndex")
+                .set(firebase.database.ServerValue.increment(1));
+            }, 500);
+          }
         }}
         onCorrectMove={() => console.log("Correct Move")}
       />
@@ -33,4 +97,26 @@ const Race: React.FC<Props> = () => {
   );
 };
 
-export default withRouter(Race);
+const CountDown: React.FC<{ onFinish: () => void }> = ({ onFinish }) => {
+  const [count, setCount] = useState(3);
+
+  const countDown = useCallback(() => {
+    setCount((x) => {
+      if (x > 1) {
+        setTimeout(countDown, 1000);
+        return x - 1;
+      } else {
+        onFinish();
+        return x;
+      }
+    });
+  }, [onFinish]);
+
+  useEffect(() => {
+    setTimeout(countDown, 1000);
+  }, [countDown]);
+
+  return <div>starting...{count}</div>;
+};
+
+export default Race;
