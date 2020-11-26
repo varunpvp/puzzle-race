83705a50d564bdab16067b57daa340ea7bc52cbd
Varun Pujari <varunpvp@gmail.com>
2020-11-26 20:30:17 +0530

    refactor race class component

--- src/pages/Race/Race.tsx
+++ src/pages/Race/Race.tsx
@@ -1,6 +1,6 @@
 import { Box, CircularProgress } from "@material-ui/core";
-import React from "react";
-import { RouteComponentProps, useParams } from "react-router-dom";
+import React, { Component } from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
 import { Auth } from "../../config/Firebase";
 import RaceType from "../../models/Race";
 import RaceEnded from "./components/RaceEnded";
@@ -11,60 +11,63 @@ import RacePlay from "./components/RacePlay";
 
 interface Props extends RouteComponentProps<{ raceId: string }> {}
 
-const Race: React.FC<Props> = () => {
-  const userId = Auth.currentUser?.uid!;
-  const params = useParams<{ raceId: string }>();
+class Race extends Component<Props> {
+  userId = Auth.currentUser?.uid!;
+  race = new RaceType(this.props.match.params.raceId, this.userId);
 
-  const race = new RaceType(params.raceId, userId);
+  render() {
+    const race = this.race;
+    const userId = this.userId;
 
-  if (race === null) {
-    return (
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="100%"
-      >
-        <CircularProgress />
-      </Box>
-    );
+    if (race === null) {
+      return (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
+        >
+          <CircularProgress />
+        </Box>
+      );
+    }
+
+    if (race.isFinished) {
+      return <RaceEnded race={race} />;
+    }
+
+    if (!race.hasRacer(userId)) {
+      return (
+        <RaceJoin
+          onJoin={async (name) => {
+            await race.joinRacer(userId, name);
+          }}
+        />
+      );
+    }
+
+    if (race.state === "waiting") {
+      return (
+        <RaceWaiting
+          userId={userId}
+          race={race}
+          onStart={() => race.startCountDown()}
+        />
+      );
+    }
+
+    if (race.state === "starting") {
+      return (
+        <RaceCountDown
+          onFinish={() => {
+            race.start();
+          }}
+        />
+      );
+    }
+
+    return <RacePlay race={race} />;
   }
+}
 
-  if (race.isFinished) {
-    return <RaceEnded race={race} />;
-  }
-
-  if (!race.hasRacer(userId)) {
-    return (
-      <RaceJoin
-        onJoin={async (name) => {
-          await race.joinRacer(userId, name);
-        }}
-      />
-    );
-  }
-
-  if (race.state === "waiting") {
-    return (
-      <RaceWaiting
-        userId={userId}
-        race={race}
-        onStart={() => race.startCountDown()}
-      />
-    );
-  }
-
-  if (race.state === "starting") {
-    return (
-      <RaceCountDown
-        onFinish={() => {
-          race.start();
-        }}
-      />
-    );
-  }
-
-  return <RacePlay race={race} />;
-};
-
-export default Race;
+export default withRouter(Race);
